35466c4a962a496ff7616b91dda004f3dd8be816
Adam Stokes <battlemidget@users.noreply.github.com>
2016-09-29 09:48:43 -0400

    More work for ssh
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- openstack-novalxd/steps/share/keypair.sh
+++ openstack-novalxd/steps/share/keypair.sh
@@ -1,5 +1,5 @@
 if ! openstack keypair show ubuntu-keypair > /dev/null 2>&1; then
     debug "adding ssh keypair"
-    openstack keypair create --public-key $SSHPUBLICKEY ubuntu-keypair
+    openstack keypair create --public-key $SSHPUBLICKEY ubuntu-keypair > /dev/null 2>&1
 fi
 exposeResult "SSH Keypair is now imported and accessible when creating compute nodes." 0 "true"
