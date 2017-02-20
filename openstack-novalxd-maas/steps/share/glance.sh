482d96e9a8b5d452064428e6e5c1b53c5f67626b
Adam Stokes <battlemidget@users.noreply.github.com>
2017-02-19 20:25:59 -0500

    Start openstack spell refactor
    We want to make sure we have novalxd and novakvm virt types for both localhost
    deploys and maas deploys.
    Also add the telemetry bits as a seperate spell for both lxd and maas
    
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

---
+++ openstack-novalxd-maas/steps/share/glance.sh
@@ -0,0 +1,32 @@
+imagetype=root.tar.xz
+diskformat=raw
+imagesuffix="-lxd"
+
+mkdir -p $HOME/glance-images || true
+if [ ! -f $HOME/glance-images/xenial-server-cloudimg-amd64-$imagetype ]; then
+    debug "Downloading xenial image..."
+    wget -qO ~/glance-images/xenial-server-cloudimg-amd64-$imagetype https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-$imagetype
+fi
+if [ ! -f $HOME/glance-images/trusty-server-cloudimg-amd64-$imagetype ]; then
+    debug "Downloading trusty image..."
+    wget -qO ~/glance-images/trusty-server-cloudimg-amd64-$imagetype https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-$imagetype
+fi
+
+if ! glance image-list --property-filter name="trusty$imagesuffix" | grep -q "trusty$imagesuffix" ; then
+    debug "Importing trusty$imagesuffix"
+    glance image-create --name="trusty$imagesuffix" \
+           --container-format=bare \
+           --disk-format=$diskformat \
+           --property architecture="x86_64" \
+           --visibility=public --file=$HOME/glance-images/trusty-server-cloudimg-amd64-$imagetype > /dev/null 2>&1
+fi
+if ! glance image-list --property-filter name="xenial$imagesuffix" | grep -q "xenial$imagesuffix" ; then
+    debug "Importing xenial$imagesuffix"
+    glance image-create --name="xenial$imagesuffix" \
+           --container-format=bare \
+           --disk-format=$diskformat \
+           --property architecture="x86_64" \
+           --visibility=public --file=$HOME/glance-images/xenial-server-cloudimg-amd64-$imagetype > /dev/null 2>&1
+fi
+
+exposeResult "Glance images for Trusty (14.04) and Xenial (16.04) are imported and accessible via Horizon dashboard." 0 "true"
