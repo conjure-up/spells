a8d08a3eab688a0e9f8b9f3a8abf0e5c7fa44140
Felipe Alfaro Solana <felipe.alfaro@gmail.com>
2017-04-04 23:43:28 +0200

    Automated deployment via conjure-up.

--- openstack-novalxd/conjure-up.sh
+++ openstack-novalxd/conjure-up.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -x
-conjure-up --apt-proxy http://ubuntu:3142 --apt-https-proxy http://ubuntu:3142 .
+conjure-up --apt-proxy http://ubuntu:3142 --apt-https-proxy http://ubuntu:3142 . localhost
