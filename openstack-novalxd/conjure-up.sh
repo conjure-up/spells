48e6871b5d7427510aa70b57287abadde71c32f5
Felipe Alfaro Solana <felipe.alfaro@gmail.com>
2017-06-13 16:23:31 +0200

    Parameterize apt cache when using conjure-up.sh.

--- openstack-novalxd/conjure-up.sh
+++ openstack-novalxd/conjure-up.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -x
-conjure-up --apt-proxy http://$(hostname):3142 --apt-https-proxy http://$(hostname):3142 $(dirname $0) localhost
+cache="192.168.122.11"
+echo conjure-up --apt-proxy http://${cache}:3142 --apt-https-proxy http://${cache}:3142 $(dirname $0) localhost
