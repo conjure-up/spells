39f6ff9a2f941bf0a550bdcd7e956cb3f251b5cc
Adam Stokes <battlemidget@users.noreply.github.com>
2017-12-18 15:16:24 -0500

    Fix security groups in neutron
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- openstack-novalxd/steps/03_neutron/neutron.sh
+++ openstack-novalxd/steps/03_neutron/neutron.sh
@@ -17,17 +17,18 @@ debug "Creating Internal Network"
 
 debug "Setting security roles"
 
+sudo apt-get update -qq > /dev/null 2>&1
 sudo apt-get install -qyf jq > /dev/null 2>&1
 clear_sec_groups=5
 i=0
 while [ $i -ne $clear_sec_groups ]; do
-    openstack security group delete "$(openstack security group list -f json|jq .[$i].ID)" >> $NEUTRON_LOG 2>&1
+    secgroup_id=$(openstack security group list -f json|jq .[$i].ID)
+    neutron security-group-rule-create --protocol icmp \
+            --direction ingress "$secgroup_id" >> $NEUTRON_LOG 2>&1
+    neutron security-group-rule-create --protocol tcp \
+        --port-range-min 22 --port-range-max 22 \
+        --direction ingress "$secgroup_id" >> $NEUTRON_LOG 2>&1
     i=$((i + 1))
 done
-neutron security-group-rule-create --protocol icmp \
-        --direction ingress default >> $NEUTRON_LOG 2>&1
-neutron security-group-rule-create --protocol tcp \
-        --port-range-min 22 --port-range-max 22 \
-        --direction ingress default >> $NEUTRON_LOG 2>&1
 
 printf "Neutron networking is now configured and is available to you during instance creation."
