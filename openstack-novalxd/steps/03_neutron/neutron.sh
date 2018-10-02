6d750307cf26d9ad365112a6dd0c46571673ca88
Chris MacNaughton <chris@centaurisolutions.nl>
2018-10-02 13:41:32 +0200

    fix security group & script layout

--- openstack-novalxd/steps/03_neutron/neutron.sh
+++ openstack-novalxd/steps/03_neutron/neutron.sh
@@ -19,16 +19,9 @@ debug "Setting security roles"
 
 sudo apt-get update -qq > /dev/null 2>&1
 sudo apt-get install -qyf jq > /dev/null 2>&1
-clear_sec_groups=5
-i=0
-while [ $i -ne $clear_sec_groups ]; do
-    secgroup_id=$(openstack security group list -f json|jq .[$i].ID)
-    neutron security-group-rule-create --protocol icmp \
-            --direction ingress "$secgroup_id" >> $NEUTRON_LOG 2>&1
-    neutron security-group-rule-create --protocol tcp \
-        --port-range-min 22 --port-range-max 22 \
-        --direction ingress "$secgroup_id" >> $NEUTRON_LOG 2>&1
-    i=$((i + 1))
-done
+
+openstack security-group-rule-create --protocol tcp --port-range-min 22 \
+          --port-range-max 22 --direction ingress default >> $NEUTRON_LOG 2>&1
+openstack security-group-rule-create --protocol icmp --direction ingress default >> $NEUTRON_LOG 2>&1
 
 printf "Neutron networking is now configured and is available to you during instance creation."
